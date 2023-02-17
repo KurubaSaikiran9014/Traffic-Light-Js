9d821b6dacf6f55df3246b318bb2b0ace957f0b5
saikiran kuruba <119987375+KurubaSaikiran9014@users.noreply.github.com>
2023-02-17 20:00:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+function stop() {
+    let butt = document.getElementById("stopButton");
+    butt.style.backgroundColor = "#cf1124";
+    let butt1 = document.getElementById("readyButton");
+    butt1.style.backgroundColor = "#1f1d41";
+    let butt2 = document.getElementById("goButton");
+    butt2.style.backgroundColor = "#1f1d41";
+    let b1 = document.getElementById("stopLight");
+    b1.style.backgroundColor = "#cf1124";
+    let b2 = document.getElementById("readyLight");
+    b2.style.backgroundColor = "#4b5069";
+    let b3 = document.getElementById("goLight");
+    b3.style.backgroundColor = "#4b5069";
+
+
+}
+
+function ready() {
+    let butt = document.getElementById("readyButton");
+    butt.style.backgroundColor = "#fcc948";
+    let butt1 = document.getElementById("stopButton");
+    butt1.style.backgroundColor = "#1f1d41";
+    let butt2 = document.getElementById("goButton");
+    butt2.style.backgroundColor = "#1f1d41";
+    let b1 = document.getElementById("readyLight");
+    b1.style.backgroundColor = "#fcc948";
+    let b2 = document.getElementById("stopLight");
+    b2.style.backgroundColor = "#4b5069";
+    let b3 = document.getElementById("goLight");
+    b3.style.backgroundColor = "#4b5069";
+
+
+}
+
+function go() {
+    let butt = document.getElementById("goButton");
+    butt.style.backgroundColor = "#199473";
+    let butt1 = document.getElementById("stopButton");
+    butt1.style.backgroundColor = "#1f1d41";
+    let butt2 = document.getElementById("readyButton");
+    butt2.style.backgroundColor = "#1f1d41";
+    let b1 = document.getElementById("goLight");
+    b1.style.backgroundColor = "#199473";
+    let b2 = document.getElementById("readyLight");
+    b2.style.backgroundColor = "#4b5069";
+    let b3 = document.getElementById("stopLight");
+    b3.style.backgroundColor = "#4b5069";
+
+}
